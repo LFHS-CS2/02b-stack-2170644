94637998c1bf9cb4c255ce16816d6e4d0c17158a
Nikko Kowalow <69934240+2170644@users.noreply.github.com>
2020-09-09 17:37:21 -0500

    Update ArrayStack.java

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,10 +1,56 @@
+import java.util.EmptyStackException;
+
 /**
  * ArrayStack should implement the Stack interface.
  * You should write your own test cases in ArrayStackTest
  *
  * @author (your name)
  */
-public class ArrayStack
+public class ArrayStack<E> implements Stack<E>
 {
+    Object[] stack = new Object[100];
+    int index = -1;
+    int size = stack.length;
 
+    public boolean isEmpty(){
+      return index == -1;
+    }
+
+    /** 
+     * Return the value that pop would give, without modifying
+     * the stack. TODO:  Throw a StackException if the stack is empty. 
+     */
+    public E peekTop(){
+      if(this.isEmpty()){
+        throw new EmptyStackException();
+      } else {
+        return (E)stack[index];
+      }
+    }
+
+    /** 
+     * Remove and return the value that has been in the stack the
+     * least time. TODO: Throw a StackException if the stack is empty. 
+     */
+    public E pop(){
+      if(this.isEmpty()){
+        throw new EmptyStackException();
+      } 
+      E ob = (E)stack[index];
+      stack[index] = null;
+      index--;
+      return ob;
+    }
+
+    /** 
+     * Add the given value to the stack. 
+     */
+    public void push (E ob){
+      if(index >= (size - 1)){
+       System.out.println("Stack is full"); 
+      } else {
+        stack[index] = ob;
+        index++; 
+      }
+    }
 }
